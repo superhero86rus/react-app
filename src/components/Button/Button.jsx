f9016902813d40af1ccd961b2b28137236e43e29
Kotov Aleksandr <kotov.alexandr@gmail.com>
2024-12-24 00:31:02 +0300

    useState

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,17 @@
 import './Button.css';
+import {useState} from 'react';
 
 function Button() {
 
-  return (
-    <button className='button accent'>Сохранить</button>
-  );
+	const [text, setText] = useState('Сохранить');
+
+	const clicked = () => {
+		setText('Закрыть');
+	};
+
+	return (
+		<button onClick={clicked} className='button accent'>{text}</button>
+	);
 }
 
 export default Button;
