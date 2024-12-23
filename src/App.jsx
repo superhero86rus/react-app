a11c17b5da696ea00e04e3de143427db8cafb670
Kotov Aleksandr <kotov.alexandr@gmail.com>
2024-12-23 19:50:26 +0300

    Первый компонент - Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
 
   return (
-   <div>Проект</div>
+    <>
+      <h1>Test</h1>
+      <p>Testing completed!</p>
+      <Button/>
+    </>
   );
 }
 
